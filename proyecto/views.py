42dbe33524134963d985a8e6662e2fc140cfd056
aerocaracas <hernandez.argenis@gmail.com>
2024-02-10 19:25:19 -0400

    update 10-02-24
    update menu

--- proyecto/views.py
+++ proyecto/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from bokitas.models import Centro
 
 # Create your views here.
 @login_required      
 def proyecto(request):
-    return render(request, 'proyecto.html')
+    centro = Centro.objects.all()
+    return render(request, 'proyecto.html',{
+        'centro': centro
+    })
