e25dcd22d06abc581672603b3a3bd9659f048537
aerocaracas <hernandez.argenis@gmail.com>
2024-07-11 16:33:24 -0400

    11-07-24 update
    11-07-24 update

--- nutricional/views.py
+++ nutricional/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
-from bokitas.models import Nutricional,Beneficiario
+from bokitas.models import Beneficiario, Nutricional
 from nutricional.forms import NutricionalForm
 from django.core.paginator import Paginator
 from datetime import datetime, date
 from dateutil import relativedelta
+from django.contrib.auth.models import User
+from nutricional.forms import NutricionalForm
+
 
 # Sesion Nutricional.
 @login_required  
 def nutricional(request):
+
     nutricionales = Nutricional.objects.all()
     page = request.GET.get('page',1)
 
     try:
-        paginator = Paginator(nutricionales, 3)
+        paginator = Paginator(nutricionales, 2)
         nutricionales = paginator.page(page)
     except:
         raise Http404
-
+    
     return render(request, 'nutricional.html',{
         'entity': nutricionales,
-        'paginator': paginator,
-
+        'paginator': paginator
     })
+
 
 
 @login_required  
