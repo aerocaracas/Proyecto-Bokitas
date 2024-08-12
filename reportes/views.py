50942a4e83db6863f9f1d3147a0d4408885de693
aerocaracas <hernandez.argenis@gmail.com>
2024-08-12 13:22:39 -0400

    12-08-24 update 111
    12-08-24 update 111

--- reportes/views.py
+++ reportes/views.py
@@ -4,6 +4,7 @@ from bokitas.models import Beneficiario, Menor, Familia, AntropBef, AntropMenor,
 from django.db.models import Q
 from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
+from datetime import datetime, date
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border, Font, PatternFill,Side
 
@@ -14,10 +15,51 @@ class exportar_proyecto(TemplateView):
     def get(self, request, *args, **kwargs):
     #####    query = Beneficiario.objects.all()
         workbook = Workbook()
-        worksheet = workbook.active
+        bandera = True
+        fecha = datetime.now()
+        fecha_fin = fecha.strftime('%d-%m-%Y - hora: %H:%m')
+        
+        titulo = ["REGISTRO BENEFICIARIOS","REGISTRO MENORES","ANTROPOMETRICOS MENORES","ANTROPOMETRICO EMBARAZADAS","ANTROPOMETRICO LACTANTE"]
 
-        worksheet.merge_cells('A1:D1')
-        worksheet.merge_cells('A2:D2')
+        for titu in titulo: 
+            if bandera:
+                worksheet = workbook.active
+                worksheet.title = titu
+                bandera = False
+                worksheet.merge_cells('A1:C1')
+                first_cell = worksheet['A1']
+                first_cell.value = "Fecha: " + fecha_fin
+            else:
+                worksheet = workbook.create_sheet(titu)
+
+    #*********  Crear encabezado en la hoja  *************
+            worksheet.merge_cells('A2:C2')
+            second_cell = worksheet['A2']
+            second_cell.value = "Bokitas"
+            second_cell.font  = Font(name = 'Tw Cen MT Condensed Extra Bold', size = 52, bold = True, color="6F42C1")
+            worksheet.merge_cells('A3:C3')
+            third_cell = worksheet['A3']
+            third_cell.value = "Bokitas Fundation  www.bokitas.org"
+            third_cell.font  = Font(name = 'Tw Cen MT Condensed Extra Bold', size = 12, bold = True, color="6F42C1")
+
+
+
+
+
+    #*********  Define los titulos de las columnas  ***************
+        columns = ['Country Name','Country Code','Year', 'Value in USD']
+        row_num = 3
+
+        # Assign the titles for each cell of the header
+        for col_num, column_title in enumerate(columns, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+            cell.fill = PatternFill("solid", fgColor="50C878")
+            cell.font  = Font(bold=True, color="F7F6FA")
+            third_cell = worksheet['D3']
+            third_cell.alignment = Alignment(horizontal="right")
+
+
 
 
 
