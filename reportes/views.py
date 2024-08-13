f452940d48ac3bd0450fffa81da009911a6338e8
aerocaracas <hernandez.argenis@gmail.com>
2024-08-12 21:20:25 -0400

    12-08-24 update 555
    12-08-24 update 555

--- reportes/views.py
+++ reportes/views.py
@@ -6,7 +6,8 @@ from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
 from datetime import datetime, date
 from openpyxl import Workbook
-from openpyxl.styles import Alignment, Border, Font, PatternFill,Side
+from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
+from openpyxl.utils import get_column_letter
 
 # Create your views here.
 
@@ -19,51 +20,59 @@ class exportar_proyecto(TemplateView):
         fecha = datetime.now()
         fecha_fin = fecha.strftime('%d-%m-%Y - hora: %H:%m')
         
-        titulo = ["REGISTRO BENEFICIARIOS","REGISTRO MENORES","ANTROPOMETRICOS MENORES","ANTROPOMETRICO EMBARAZADAS","ANTROPOMETRICO LACTANTE"]
+        hojas = ["REGISTRO BENEFICIARIOS","REGISTRO MENORES","ANTROPOMETRICOS MENORES","ANTROPOMETRICO EMBARAZADAS","ANTROPOMETRICO LACTANTE"]
 
-        for titu in titulo: 
+        for hoja in hojas: 
             if bandera:
                 worksheet = workbook.active
-                worksheet.title = titu
+                worksheet.title = hoja
                 bandera = False
                 worksheet.merge_cells('A1:C1')
                 first_cell = worksheet['A1']
                 first_cell.value = "Fecha: " + fecha_fin
             else:
-                worksheet = workbook.create_sheet(titu)
+                worksheet = workbook.create_sheet(hoja)
 
     #*********  Crear encabezado en la hoja  *************
-            worksheet.merge_cells('A2:C2')
+            worksheet.merge_cells('A2:D2')
             second_cell = worksheet['A2']
             second_cell.value = "Bokitas"
             second_cell.font  = Font(name = 'Tw Cen MT Condensed Extra Bold', size = 52, bold = True, color="6F42C1")
-            worksheet.merge_cells('A3:C3')
+
+            worksheet.merge_cells('A3:D3')
             third_cell = worksheet['A3']
             third_cell.value = "Bokitas Fundation  www.bokitas.org"
             third_cell.font  = Font(name = 'Tw Cen MT Condensed Extra Bold', size = 12, bold = True, color="6F42C1")
+        
+    #*********  hoja de Datos de los Beneficiario  *************
+        worksheet = workbook['REGISTRO BENEFICIARIOS']
+        worksheet.merge_cells('A4:U6')
+        fourth_cell = worksheet['A4']
+        fourth_cell.value = "REGISTRO DE LOS BENEFICIARIOS"
+        fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
+        fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
 
+    #*********  Registro de Datos de los Beneficiario  *************
+        titulo1 = ['PROYECTO','CENTRO','CÉDULA','NOMBRE','APELLIDO','SEXO','FECHA NAC.','EDAD','MESES','EMBARAZADA','LACTANDO','ESTADO CIVIL','EDUCACIÓN','PROFESIÓN','LABORAL','TELEFONO','CORREO','DIRECCIÓN','CIUDAD','ESTADO','ESTATUS','NÚMERO DE CUENTA']
+        row_num = 7
+        thin = Side(border_style="thin", color="000000")
+        double = Side(border_style="double", color="000000")
 
-
-
-
-    #*********  Define los titulos de las columnas  ***************
-        columns = ['Country Name','Country Code','Year', 'Value in USD']
-        row_num = 3
-
-        # Assign the titles for each cell of the header
-        for col_num, column_title in enumerate(columns, 1):
+    #********* asigna el titulo a las columnas  ************************
+        for col_num, column_title in enumerate(titulo1, 1):
             cell = worksheet.cell(row=row_num, column=col_num)
             cell.value = column_title
-            cell.fill = PatternFill("solid", fgColor="50C878")
-            cell.font  = Font(bold=True, color="F7F6FA")
-            third_cell = worksheet['D3']
-            third_cell.alignment = Alignment(horizontal="right")
+            cell.fill = PatternFill("solid", fgColor="E2D9F3")
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=double)
+            cell.font  = Font(bold=True, size = 14, color="333399")
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            worksheet.column_dimensions[get_column_letter(col_num)].auto_size=True
+            
 
 
 
 
 
-        
     #*********  Establecer el nombre del Archivo *******
         nombre_archvo = "Reporte_por_Proyecto.xlsx"
     
