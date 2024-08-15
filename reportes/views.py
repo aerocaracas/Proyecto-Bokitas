b6b4a31ca5759024d77f8bef98ad9a6635e6b3d2
aerocaracas <hernandez.argenis@gmail.com>
2024-08-15 19:30:09 -0400

    15-08-24 update 9999
    15-08-24 update 9999

--- reportes/views.py
+++ reportes/views.py
@@ -308,7 +308,7 @@ class exportar_beneficiario_detalle(TemplateView):
         fecha = datetime.now()
         fecha_fin = fecha.strftime('%d-%m-%Y - hora: %H:%m') 
         worksheet = workbook.active
-        worksheet.title = "REGISTRO BENEFICIARIOS"
+        worksheet.title = "REGISTRO BENEFICIARIO"
         worksheet.merge_cells('A1:C1')
         first_cell = worksheet['A1']
         first_cell.value = "Fecha: " + fecha_fin
@@ -331,7 +331,7 @@ class exportar_beneficiario_detalle(TemplateView):
        # worksheet = workbook['REGISTRO BENEFICIARIOS']
         worksheet.merge_cells('A4:W6')
         fourth_cell = worksheet['A4']
-        fourth_cell.value = "REGISTRO DE LOS BENEFICIARIOS"
+        fourth_cell.value = "REGISTRO DEL BENEFICIARIO"
         fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
         fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
 
@@ -364,6 +364,217 @@ class exportar_beneficiario_detalle(TemplateView):
                 cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
                 if col_num == 6 or col_num == 7 or col_num == 8 or col_num == 9 or col_num == 10 or col_num == 11 or col_num == 12 or col_num == 22:
                     cell.alignment = Alignment(horizontal="center")
+
+
+    
+    #*********  Registro de Datos antropometricos Menores  *************
+
+        row_num = row_num + 3
+        xx = 'A'+ str(row_num)
+        yy = 'V'+ str(row_num + 2)
+        worksheet.merge_cells(xx +':'+ yy)
+        fourth_cell = worksheet[xx]
+        fourth_cell.value = "REGISTRO de MENORES"
+        fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
+        fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
+
+        imc_menores = AntropMenor.objects.filter(cedula_bef_id = pk).order_by('cedula_id').select_related('cedula')
+
+        titulos = ['PROYECTO','CÉDULA','NOMBRE','APELLIDO','SEXO','FECHA NAC.','EDAD','MESES','PESO ACTUAL','TALLA ACTUAL','FECHA EVALUACIÓN','EDAD','MESES','PESO','TALLA','CBI','PTR','PSE','CC','IMC','DIAGNOSTICO PESO','DIAGNOSTICO TALLA']
+        thin = Side(border_style="thin", color="000000")
+        double = Side(border_style="double", color="000000")
+        row_num = row_num + 3
+    #********* asigna el titulo a las columnas  ************************
+        for col_num, column_title in enumerate(titulos, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+            cell.fill = PatternFill("solid", fgColor="E2D9F3")
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=double)
+            cell.font  = Font(bold=True, size = 14, color="333399")
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            if col_num == 21 or col_num == 22: 
+                adjusted_width = (len(cell.value) + 10) * 1.2
+                worksheet.column_dimensions[get_column_letter(col_num)].width = adjusted_width
+
+    #**************  Agrega la data a las celdas
+        xCedula = 0
+
+        for imc in imc_menores:
+            row_num += 1
+            datos = [imc.proyecto,imc.cedula.cedula,imc.cedula.nombre,imc.cedula.apellido,imc.cedula.sexo,imc.cedula.fecha_nac.strftime('%d-%m-%Y'),imc.edad,imc.meses,imc.cedula.peso_actual,imc.cedula.talla_actual,imc.fecha.strftime('%d-%m-%Y'),imc.edad,imc.meses,imc.peso,imc.talla,imc.cbi,imc.ptr,imc.pse,imc.cc,imc.imc,imc.diagnostico,imc.diagnostico_talla]
+            
+            if imc.cedula.cedula != xCedula:
+                for col_num, cell_value in enumerate(datos, 1):
+                    cell = worksheet.cell(row=row_num, column=col_num)
+                    cell.value = str(cell_value)
+                    cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                    cell.alignment = Alignment(horizontal="center")
+                    if col_num == 1 or col_num == 2 or col_num == 3 or col_num == 4 or col_num == 5 or col_num == 21 or col_num == 22:
+                        cell.alignment = Alignment(horizontal="left")
+
+                xCedula = imc.cedula.cedula
+            else:
+                for col_num, cell_value in enumerate(datos, 1):
+                    if col_num > 10:
+                        cell = worksheet.cell(row=row_num, column=col_num)
+                        cell.value = str(cell_value)
+                        cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                        cell.alignment = Alignment(horizontal="center")
+                        if col_num == 21 or col_num == 22:
+                            cell.alignment = Alignment(horizontal="left")
+
+
+
+
+    #*********  Registro de Datos antropometricos Beneficiario *************
+    
+        row_num = row_num + 3
+        xx = 'A'+ str(row_num)
+        yy = 'O'+ str(row_num + 2)
+        worksheet.merge_cells(xx +':'+ yy)
+        fourth_cell = worksheet[xx]
+        fourth_cell.value = "REGISTRO ANTROPOMÉTRICO DEL BENEFICIARIO"
+        fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
+        fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
+
+        imc_beneficiario = AntropBef.objects.filter(cedula_bef_id = pk).order_by('cedula_bef_id').select_related('cedula_bef')
+
+        titulos = ['PROYECTO','CÉDULA','NOMBRE','APELLIDO','SEXO','FECHA NAC.','FECHA EVAL.','ESTADO','EDAD','MESES','PESO','TALLA','CBI','IMC','DIAGNOSTICO PESO']
+        thin = Side(border_style="thin", color="000000")
+        double = Side(border_style="double", color="000000")
+        row_num = row_num + 3
+    #********* asigna el titulo a las columnas  ************************
+        for col_num, column_title in enumerate(titulos, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+            cell.fill = PatternFill("solid", fgColor="E2D9F3")
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=double)
+            cell.font  = Font(bold=True, size = 14, color="333399")
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            if col_num == 21 or col_num == 22: 
+                adjusted_width = (len(cell.value) + 10) * 1.2
+                worksheet.column_dimensions[get_column_letter(col_num)].width = adjusted_width
+
+    #**************  Agrega la data a las celdas
+        xCedula = 0
+
+        for imc in imc_beneficiario:
+            row_num += 1
+            datos = [imc.cedula_bef.proyecto,imc.cedula_bef.cedula,imc.cedula_bef.nombre,imc.cedula_bef.apellido,imc.cedula_bef.sexo,imc.cedula_bef.fecha_nac.strftime('%d-%m-%Y'),imc.fecha.strftime('%d-%m-%Y'),imc.embarazo_lactando,imc.edad,imc.meses,imc.peso,imc.talla,imc.cbi,imc.imc,imc.diagnostico]
+            
+            if imc.cedula_bef != xCedula:
+                for col_num, cell_value in enumerate(datos, 1):
+                    cell = worksheet.cell(row=row_num, column=col_num)
+                    cell.value = str(cell_value)
+                    cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                    cell.alignment = Alignment(horizontal="left")
+                    if col_num == 6 or col_num == 7 or col_num == 8 or col_num == 9 or col_num == 10 or col_num == 11 or col_num == 12 or col_num == 13 or col_num == 14:
+                        cell.alignment = Alignment(horizontal="center")
+
+                xCedula = imc.cedula_bef
+            else:
+                for col_num, cell_value in enumerate(datos, 1):
+                    if col_num > 6:
+                        cell = worksheet.cell(row=row_num, column=col_num)
+                        cell.value = str(cell_value)
+                        cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                        cell.alignment = Alignment(horizontal="center")
+                        if col_num == 15:
+                            cell.alignment = Alignment(horizontal="left")
+
+
+    #*********  Registro entrega de Medicamentos / Productos al Beneficiario *************
+    
+        row_num = row_num + 3
+        xx = 'A'+ str(row_num)
+        yy = 'H'+ str(row_num + 2)
+        worksheet.merge_cells(xx +':'+ yy)
+        fourth_cell = worksheet[xx]
+        fourth_cell.value = "REGISTRO DE ENTREGA DE MEDICAMENTOS / PRODUCTOS"
+        fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
+        fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
+
+        medicamentos = Medicamento.objects.filter(cedula_bef_id = pk).order_by('cedula_bef_id').select_related('cedula_bef')
+
+        titulos = ['PROYECTO','CÉDULA','NOMBRE','APELLIDO','FECHA ENTREGA','NOMBRE.',' DESCRIPCIÓN ','CANTIDAD']
+        thin = Side(border_style="thin", color="000000")
+        double = Side(border_style="double", color="000000")
+        row_num = row_num + 3
+    #********* asigna el titulo a las columnas  ************************
+        for col_num, column_title in enumerate(titulos, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+            cell.fill = PatternFill("solid", fgColor="E2D9F3")
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=double)
+            cell.font  = Font(bold=True, size = 14, color="333399")
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            if col_num == 5 or col_num == 7: 
+                adjusted_width = (len(cell.value) + 10) * 1.2
+                worksheet.column_dimensions[get_column_letter(col_num)].width = adjusted_width
+
+    #**************  Agrega la data a las celdas
+        xCedula = 0
+
+        for med in medicamentos:
+            row_num += 1
+            datos = [med.cedula_bef.proyecto,med.cedula_bef.cedula,med.cedula_bef.nombre,med.cedula_bef.apellido,med.fecha.strftime('%d-%m-%Y'),med.nombre,med.descripcion,med.cantidad]
+            
+            
+            for col_num, cell_value in enumerate(datos, 1):
+                cell = worksheet.cell(row=row_num, column=col_num)
+                cell.value = str(cell_value)
+                cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                cell.alignment = Alignment(horizontal="left")
+                if col_num == 5 or col_num == 8:
+                    cell.alignment = Alignment(horizontal="center")
+
+
+
+    #*********  Registro Familiares del Beneficiario *************
+    
+        row_num = row_num + 3
+        xx = 'A'+ str(row_num)
+        yy = 'L'+ str(row_num + 2)
+        worksheet.merge_cells(xx +':'+ yy)
+        fourth_cell = worksheet[xx]
+        fourth_cell.value = "REGISTRO DE FAMILIARES DEL BENEFICIARIO"
+        fourth_cell.font  = Font(name = 'Tahoma', size = 16, bold = True, color="333399")
+        fourth_cell.alignment = Alignment(horizontal="center", vertical="center")      
+
+        familiares = Familia.objects.filter(cedula_bef_id = pk)
+
+        titulos = ['PARENTESCO','CÉDULA','NOMBRE','APELLIDO','SEXO','FECHA NAC.','EDAD','MESES','ESTADO CIVIL',' EDUCACIÓN ','PROFESIÓN','LABORAL']
+        thin = Side(border_style="thin", color="000000")
+        double = Side(border_style="double", color="000000")
+        row_num = row_num + 3
+    #********* asigna el titulo a las columnas  ************************
+        for col_num, column_title in enumerate(titulos, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+            cell.fill = PatternFill("solid", fgColor="E2D9F3")
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=double)
+            cell.font  = Font(bold=True, size = 14, color="333399")
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            if col_num == 5 or col_num == 7: 
+                adjusted_width = (len(cell.value) + 10) * 1.2
+                worksheet.column_dimensions[get_column_letter(col_num)].width = adjusted_width
+
+    #**************  Agrega la data a las celdas
+        xCedula = 0
+
+        for fam in familiares:
+            row_num += 1
+            datos = [fam.parentesco,fam.cedula,fam.nombre,fam.apellido,fam.sexo,fam.fecha_nac.strftime('%d-%m-%Y'),fam.edad,fam.meses,fam.estado_civil,fam.educacion,fam.profesion,fam.laboral]
+            
+            for col_num, cell_value in enumerate(datos, 1):
+                cell = worksheet.cell(row=row_num, column=col_num)
+                cell.value = str(cell_value)
+                cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+                cell.alignment = Alignment(horizontal="left")
+                if col_num == 6 or col_num == 7 or col_num == 8:
+                    cell.alignment = Alignment(horizontal="center")
+
+
 
     #*********  Establecer el nombre del Archivo *******
         nombre_archvo = "Reporte_Beneficiario_detalle.xlsx"
