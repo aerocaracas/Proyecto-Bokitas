f44bd16db6eb464633cc6b7fb991800fa944a972
aerocaracas <hernandez.argenis@gmail.com>
2024-08-24 10:29:16 -0400

    23-08-24 ipdate 55555
    23-08-24 ipdate 55555

--- proyecto/forms.py
+++ proyecto/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from bokitas.models import Proyecto,Jornada 
-from django.forms.widgets import NumberInput 
+from django.forms.widgets import NumberInput
+from django.views.generic import FormView 
 
 class ProyectoForm(forms.ModelForm):
     class Meta:
@@ -22,7 +23,7 @@ class ExpProyectoForm(forms.ModelForm):
 class ExpJornadaForm(forms.ModelForm):
     class Meta:
         model = Jornada
-        fields = ('proyecto', 'jornada')
+        fields = ('proyecto','jornada')
 
         labels = {'proyecto':'Seleccione el Proyecto','jornada':'Seleccione la Jornada'}
         
@@ -34,16 +35,22 @@ class ExpJornadaForm(forms.ModelForm):
             try:
                 proyecto_value = self.data.get('proyecto')
                 self.fields['jornada'].queryset = Jornada.objects.filter(proyecto_id=proyecto_value)
+                print(self.fields['jornada'].queryset)
             except (ValueError, TypeError):
                 pass
 
     def clean_jornada(self):
         jornada_value = self.cleaned_data.get('jornada')
-        
         if not jornada_value:
             raise ExpJornadaForm.ValidationError('Debe seleccionar un valor para la Jornada')
         return jornada_value
     
+
+class EditarFormView(FormView):
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['jornada'] = self.request.GET.get('jornada')
+        return kwargs
 
 
 
